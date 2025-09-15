7c51e8fc801985b2c8e9026dd4d9ce7ed3dec1e1
psyew2 <psyew2@nottingham.ac.uk>
2025-09-15 14:59:31 +0100

    Change export

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/my-app",
+  output: "export",
+  reactStrictMode: true
 };
 
-export default nextConfig;
+module.exports = nextConfig;
